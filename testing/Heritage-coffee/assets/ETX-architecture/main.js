1ebd7e2ca1eb3515c8738fda4c339693185b3daf
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2023-09-13 16:24:29 -0500

    commit__28
    Updated coffee shop website with swiperJS functionality and enabled the get directions button

--- testing/Heritage-coffee/assets/ETX-architecture/main.js
+++ testing/Heritage-coffee/assets/ETX-architecture/main.js
@@ -31,3 +31,61 @@ const scrollActive = () =>{
 	})
 }
 window.addEventListener('scroll', scrollActive)
+
+
+/*==================== SWIPER ====================*/
+var swiper = new Swiper(".featured__container", {
+
+  
+    effect: "cube",
+      grabCursor: true,
+	  loop: true,
+      cubeEffect: {
+        shadow: true,
+	  },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // breakpoints:{
+    //   700: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+
+    //     coverflowEffect: {
+    //       rotate: 50,
+    //       stretch: 0,
+    //       depth: 100,
+    //       modifier: 1,
+    //       slideShadows: true,
+    //     },
+    //   },
+    // }
+
+    
+  });
+
+  // Select the button and the map overlay element
+const openButton = document.getElementById("map-open");
+const mapOverlay = document.querySelector(".map-bkgr");
+const closeButton = document.querySelector(".open-icon#map-close"); // Corrected selector
+
+  // Function to toggle the map overlay visibility
+function toggleMapOverlay() {
+    if (mapOverlay.style.bottom === "0px") {
+        mapOverlay.style.bottom = "-100%";
+    } else {
+        mapOverlay.style.bottom = "0px";
+    }
+}
+
+// Function to close the map overlay
+function closeMapOverlay() {
+    mapOverlay.style.bottom = "-100%";
+}
+
+// Add click event listeners to the open and close buttons
+openButton.addEventListener("click", toggleMapOverlay);
+closeButton.addEventListener("click", closeMapOverlay);
