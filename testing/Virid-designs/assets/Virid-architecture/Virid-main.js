584e4f01b5956d7535350e9e6c07193486755b01
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-03-19 14:34:24 -0500

    commit__76
    Added Virid designs website to the testing environment

---
+++ testing/Virid-designs/assets/Virid-architecture/Virid-main.js
@@ -0,0 +1,38 @@
+
+
+/*=============== NAVIGATION CONTROLS ===============*/
+
+const navMenu = document.getElementById('nav--menu'),
+navToggle = document.getElementById('nav--open'),
+navClose = document.getElementById('nav--close')
+
+if(navToggle){
+navToggle.addEventListener('click', () =>{
+navMenu.classList.add('show-nav')
+})
+}
+
+if(navClose){
+navClose.addEventListener('click', () =>{
+navMenu.classList.remove('show-nav')
+})
+}
+
+const navlink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+const navMenu = document.getElementById('nav--menu')
+navMenu.classList.remove('show-nav')
+}
+navlink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        :header.classList.remove('blur-header')
+  }
+  
+  window.addEventListener('scroll' , blurHeader)
