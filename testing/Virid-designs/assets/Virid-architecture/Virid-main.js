004c4061e9c90e2e23da26a4956bbd97b5007fc9
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-04-02 22:44:55 -0500

    commit__79
    Updated to navigation bar for the Virid design website

--- testing/Virid-designs/assets/Virid-architecture/Virid-main.js
+++ testing/Virid-designs/assets/Virid-architecture/Virid-main.js
@@ -2,44 +2,36 @@
 
 /*=============== NAVIGATION CONTROLS ===============*/
 
-const navMenu = document.getElementById('nav--menu'),
-navToggle = document.getElementById('nav--open'),
-navClose = document.getElementById('nav--close')
+const navMenu = document.getElementById("nav-list")
+const toggleNavBtn = document.getElementById("nav-toggle-btn")
 
-if(navToggle){
-navToggle.addEventListener('click', () =>{
-navMenu.classList.add('show-nav')
-})
+if(toggleNavBtn){
+  toggleNavBtn.addEventListener('click', () =>{
+    navMenu.classList.toggle('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+  })
 }
 
-if(navClose){
-navClose.addEventListener('click', () =>{
-navMenu.classList.remove('show-nav')
-})
-}
-
-const navlink = document.querySelectorAll('.nav__link')
+const navlink = document.querySelectorAll('.nav-list-item')
 
 const linkAction = () =>{
-const navMenu = document.getElementById('nav--menu')
-navMenu.classList.remove('show-nav')
+const navMenu = document.getElementById('nav-list')
+    navMenu.classList.remove('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+
 }
 navlink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-const blurHeader = () => {
-    const header = document.getElementById('header')
-  
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-                        :header.classList.remove('blur-header')
-  }
-  
-  window.addEventListener('scroll' , blurHeader)
 
-  /*=============== HOME PARALLAX ===============*/
-document.addEventListener('DOMContentLoaded', function () {
-	// Initialize Rellax
-	var rellax = new Rellax('.home-background-img');
+/*=============== HOME PARALLAX ===============*/
+ document.addEventListener('DOMContentLoaded', function () {
+ 	// Initialize Rellax
+ 	var rellax = new Rellax('.home-background-img');
 
-});
+ });
+
+
+
+
+
