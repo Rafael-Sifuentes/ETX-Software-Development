affabc92088d8eaf6c70983c98a8dd1aa62ce4e5
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-06-07 01:00:44 -0500

    commit__97
    Updated lawn care website to be fully responsive across all devices

--- testing/lawn-care/assets/ETX-routes/main.js
+++ testing/lawn-care/assets/ETX-routes/main.js
@@ -129,3 +129,32 @@ function cycleTestimonials(containerSelector, prevButtonSelector, nextButtonSele
 
 // Example usage: Assuming your container has class "testemonial-container"
 cycleTestimonials(".testemonial-container", "#button-prev", "#button-next");
+
+/*=============== SCROLL REVEAL JS ===============*/
+const sr = ScrollReveal({
+	distance: '50px',
+	duration: 2500,
+  })
+  
+  sr.reveal(`.top`,{
+	origin: 'top',
+  })
+
+  sr.reveal(`.bottom`,{
+	origin: 'bottom',
+  })
+
+  sr.reveal(`.bottom-delayed`,{
+    origin: 'bottom',
+    delay: 400,
+    interval: 350,
+
+  })
+
+  sr.reveal(`.left`,{
+    origin: 'left',
+  })
+
+  sr.reveal(`.right`,{
+    origin: 'right',
+  })
