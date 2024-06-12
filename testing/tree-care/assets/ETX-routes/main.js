a55905d6e9432dbd130d33bbab4b879a6b56a052
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-06-12 11:25:14 -0500

    commit__98
    Cleared space in the testing environment and added tree-care website.

---
+++ testing/tree-care/assets/ETX-routes/main.js
@@ -0,0 +1,91 @@
+
+
+const navMenu = document.getElementById("nav-list")
+const toggleNavBtn = document.getElementById("nav-toggle-btn")
+
+if(toggleNavBtn){
+  toggleNavBtn.addEventListener('click', () =>{
+    navMenu.classList.toggle('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+  })
+}
+
+const navlink = document.querySelectorAll('.nav-list-item')
+
+const linkAction = () =>{
+const navMenu = document.getElementById('nav-list')
+    navMenu.classList.remove('show-nav')
+    toggleNavBtn.classList.toggle('rotate')
+
+}
+navlink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const blurHeader = () => {
+    const header = document.getElementById('header')
+  
+    this.scrollY >= 50 ? header.classList.add('header-fill')
+                        :header.classList.remove('header-fill')
+  }
+  
+  window.addEventListener('scroll' , blurHeader)
+
+
+/*=============== SCROLL SECTIONS ACTIVE ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav-list a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link');
+		}else{
+			sectionsClass.classList.remove('active-link');
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+var rellax = new Rellax('.home-image-container',{
+	center:true
+});
+
+
+
+/*=============== SCROLL REVEAL JS ===============*/
+const sr = ScrollReveal({
+	distance: '50px',
+	duration: 2500,
+  })
+  
+  sr.reveal(`.top`,{
+	origin: 'top',
+  })
+
+  sr.reveal(`.bottom`,{
+	origin: 'bottom',
+  })
+
+  sr.reveal(`.bottom-delayed`,{
+    origin: 'bottom',
+    delay: 400,
+    interval: 350,
+
+  })
+
+  sr.reveal(`.left`,{
+    origin: 'left',
+  })
+
+  sr.reveal(`.right`,{
+    origin: 'right',
+  })
