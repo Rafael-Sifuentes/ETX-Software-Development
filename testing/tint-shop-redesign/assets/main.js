f6c8de524759ffb728c9378efa53fa695d07e1e7
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-11-09 10:19:53 -0600

    commit__126
    Updates to the tint shop redesign

--- testing/tint-shop-redesign/assets/main.js
+++ testing/tint-shop-redesign/assets/main.js
@@ -13,3 +13,30 @@ function scrollToSection(sectionId) {
       section.scrollIntoView({ behavior: 'smooth' }); 
     }
 }
+
+
+
+// GALLERY SWIPER JS CONTROLS
+var swiper = new Swiper('.gallery-swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    speed: 800,
+    loop: true,
+  
+     autoplay: {
+         delay: 2000,
+         disableOnInteraction: false,
+       },
+  
+      navigation: {
+        nextEl: '.gallery-next',
+        prevEl: '.gallery-prev',
+    },
+    
+       breakpoints: {
+  
+         1000: {
+           slidesPerView: 3,
+         },
+     },
+  });
