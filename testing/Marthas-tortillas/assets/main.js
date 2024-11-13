0a3728d56a55d2c2bbd66364005ac032f7a3441d
Rafael-Sifuentes <110879082+Rafael-Sifuentes@users.noreply.github.com>
2024-11-13 12:28:36 -0600

    commit__131
    Added marthas website to the testing site

---
+++ testing/Marthas-tortillas/assets/main.js
@@ -0,0 +1,56 @@
+
+
+// SWIPER JS
+
+var swiper = new Swiper('.featured-swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    speed: 800,
+    loop: true,
+  
+     autoplay: {
+         delay: 2000,
+         disableOnInteraction: true,
+       },
+  
+      navigation: {
+        nextEl: '.featured-next',
+        prevEl: '.featured-prev',
+    },
+    
+       breakpoints: {
+
+        744:{
+            slidesPerView: 2,
+        },
+  
+         1080: {
+           slidesPerView: 3,
+         },
+     },
+  });
+
+
+// MENU SELECTION / FILTER JS
+let container = document.querySelector('.menu');
+var mixer;
+
+if (container) {
+    mixer = mixitup('.menu', {
+      selectors: {
+        target: '.menu-catagory'
+      },
+      animation: {
+        duration: 400
+      }
+    });
+  }
+  
+  const linkProducts = document.querySelectorAll('.menu-selection-item'); 
+  
+  linkProducts.forEach(l => l.addEventListener('click', activeProduct)); 
+  
+  function activeProduct() {
+    linkProducts.forEach(l => l.classList.remove('active-featured')); 
+    this.classList.add('active-featured'); 
+  }
